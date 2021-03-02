e7e4f595cda4baa9fdfa650c793da5ccbc35603a
Jason Taisey <jdtaisey89@icloud.com>
2021-03-01 18:01:46 -0800

    Feature 2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // ============= Packages =========
 const express = require('express');
 require('dotenv').config();
@@ -9,21 +9,28 @@ const app = express();//express() returns a fully ready to run server object
 const PORT = process.env.PORT;
 
 // ============= Variables ==============
-const paperback = ['books'];
-app.set('view engine', 'ejs');
+const bookCreate = {
+  title: 'Jasons Book',
+  author: 'Jason',
+  description: 'A thrilling novel',
+};
+const paperback = [bookCreate];
 
 // ============= Access ==============
 app.use(express.static('./public')); // server all the files in the specified folder
 app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 // ============= Routes ==============
-app.get('/searches/new', (req, res) => {
-  res.render(paperback); // .send will update and send information now we want to .render. First one to be searches and in the parens pass a string of the filename 
+app.get('/hello', (res) =>{res.render('pages/index'); // .send will update and send information now we want to .render. First one to be searches and in the parens pass a string of the filename
 });
 
-app.get('/hello', (req, res) =>
-{res.render('pages/index'); // .send will update and send information now we want to .render. First one to be searches and in the parens pass a string of the filename 
+app.get('/searches/new', (req, res) => {
+  res.render('pages/searches/new.ejs', {paperback}); // .send will update and send information now we want to .render. First one to be searches and in the parens pass a string of the filename 
 });
+
+
+// function bookCreate (object)
 
 // ============= Post ==============
 app.post('/create-books', (req, res) => { //create-books will target the HTML file
